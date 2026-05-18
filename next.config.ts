d80676633dbe4aad123392e96f46d6119ce9f2e4
Muhammad Hidayat Mauluddin <mauludindayat@gmail.com>
2026-05-18 20:19:25 +0700

    delete reverse proxy on next config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/portfolio-chatbot/:path*',
-        destination: 'http://portfolio-chatbot.hidayatmauluddin.my.id:50271/:path*',
-      },
-    ];
-  },
-  // You can also add other configurations here if needed
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
